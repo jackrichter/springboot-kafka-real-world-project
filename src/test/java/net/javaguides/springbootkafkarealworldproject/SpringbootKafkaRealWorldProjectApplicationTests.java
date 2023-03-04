8e53c16698b340be66573d1fd99c4308821bd054
jackrichter <jackrichter1@gmail.com>
2023-03-04 04:34:12 +0100

    Basic demonstration of topic, producer, consumer of a stream av data and database storage.

---
+++ src/test/java/net/javaguides/springbootkafkarealworldproject/SpringbootKafkaRealWorldProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaguides.springbootkafkarealworldproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaRealWorldProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
